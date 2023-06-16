025b9023cdde19d6f1a0e25afd22b5d8f0d04346
leopard1982 <99190737+leopard1982@users.noreply.github.com>
2023-06-16 14:37:12 +0700

    tambah dikit

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,9 +1,10 @@
 
 from django.urls import path, re_path
 
-from blogs.views import homepage, admin_page
+from blogs.views import homepage, admin_page,logoutme
 
 urlpatterns = [
     path('', homepage, name="homepage"),
-    path('adm/',admin_page,name="admin_page")
+    path('adm/',admin_page,name="admin_page"),
+    path('logout/',logoutme,name="logoutme"),
 ]
--- blogs/views.py
+++ blogs/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 from blogs.forms import formBlogs
 from blogs.models import myBlogs
 from django.core.paginator import Paginator
+
+from django.contrib.auth import login,logout,authenticate
 
 # Create your views here.
 def homepage(request):
@@ -24,11 +26,31 @@ def homepage(request):
 	return render(request,'blogs/dashboard.html',{'olddata':lihatlah,'newdata':newdata})
 
 def admin_page(request):
-	if request.method == "POST":
-		forms = formBlogs(request.POST, request.FILES)
-		if forms.is_valid():
-			forms.save()
-		else:
-			print(forms)
-	forms = formBlogs()
-	return render(request,'blogs/input_admin.html',{'forms':forms})
+	username=None
+	password=None
+	try:
+		if request.method=="POST":
+			username = request.POST['txtUsername']
+			password = request.POST['txtPassword']
+	except:
+		pass
+
+	if username!=None:
+		user=authenticate(username=username,password=password)
+		if user is not None:
+			login(request,user)
+		return HttpResponseRedirect('/blogs/adm/')
+		print(username)
+	else:
+		if request.method == "POST":
+			forms = formBlogs(request.POST, request.FILES)
+			if forms.is_valid():
+				forms.save()
+			else:
+				print(forms)
+		forms = formBlogs()
+		return render(request,'blogs/input_admin.html',{'forms':forms})
+
+def logoutme(request):
+	logout(request)
+	return HttpResponseRedirect('/blogs/adm/')
