13bddaee64a0b61b68ffcce8da41e75184b87634
ladame <salegrandmont@gmail.com>
2023-07-16 02:25:08 +0200

    Change all Remouille to Château-Thébaud

--- perancis/urls.py
+++ perancis/urls.py
@@ -23,7 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('landingpage.urls')),
-    path('blogs/',include('blogs.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, kwargs={'document_root': settings.MEDIA_ROOT}),
     re_path(r'^static/(?P<path>.*)$', serve, kwargs={'document_root': settings.STATICFILES_DIRS}),
 ]
